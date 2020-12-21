d399a71eb3b66638545aa537ce817d22739a56e5
Daniel <dksmith328@gmail.com>
2020-12-21 15:26:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Imports
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Listen on Port 5000
+app.listen(port, () => console.info(`App listening on port ${port}`))
+
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/images'))
+
+// Set View's
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// Navigation
+app.get('', (req, res) => {
+   res.sendFile(__dirname + '/views/index.html')
+})
---
+++ public/js/index.js
@@ -0,0 +1,63 @@
+  $(document).ready(function() {
+
+    window.onload = function() {
+      var search = sessionStorage.getItem("search");
+      console.log(search)
+
+      if(search == null || search == '')
+      {
+        setSearch();
+      }
+      else
+      {
+        var url = '';
+        var img = '';
+        var title = '';
+        var author = '';
+        var div= '';
+
+        $.get('https://www.googleapis.com/books/v1/volumes?q=author:"' + search + '"' + "&filter=ebooks&langRestrict=en&maxResults=40&orderBy=newest",function(response) {
+
+          for(i=0; i<response.items.length;i++)
+          {
+            //get title of the book
+            div=$('<div class="col-1-10"><a href=' + response.items[i].volumeInfo.infoLink + ' target="_blank"><img src=' + response.items[i].volumeInfo.imageLinks.thumbnail + '></a><p>'+ response.items[i].volumeInfo.title + '</p></div>')
+
+            div.appendTo("#result");
+          }
+        })
+      }
+    };
+    function setSearch() {
+        sessionStorage.setItem("search", $("#author").val());
+    };
+    $("#search").submit(function() {
+      sessionStorage.setItem("search", $("#author").val());
+    });
+    $('.search').click(function() {
+        $('.search').addClass('active');
+        $('.line-1').css({
+            'transform': 'rotate(45deg)',
+            'top': '0px',
+            'left': '0px'
+        });
+        $('.line-2').css({
+            'height':'40px',
+            'opacity':'1'
+        });
+    });
+    $('.line-1, .line-2').click(function() {
+        $('.search').removeClass('active').val('');
+        $('.line-1').css({
+            'transform': 'rotate(-45deg)',
+            'top': '-20px',
+            'left': '45px'
+        });
+        $('.line-2').css({
+            'height':'0px',
+            'opacity':'0'
+        });
+
+    });
+    return false;
+});
